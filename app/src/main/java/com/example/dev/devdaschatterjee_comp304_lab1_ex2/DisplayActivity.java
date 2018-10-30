0927f2fb2cd9b0000306be51ed5f31396bab9fe7
devdasc <devdasc777@gmail.com>
2018-10-29 21:38:44 -0400

    Initial commit

---
+++ app/src/main/java/com/example/dev/devdaschatterjee_comp304_lab1_ex2/DisplayActivity.java
@@ -0,0 +1,18 @@
+package com.example.dev.devdaschatterjee_comp304_lab1_ex2;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+public class DisplayActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display);
+
+
+    }
+}
---
+++ app/src/main/java/com/example/dev/devdaschatterjee_comp304_lab1_ex2/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.dev.devdaschatterjee_comp304_lab1_ex2;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+    // to display the Display activity
+    public void btnDisplay_ClickHandler(View v){
+
+          Intent intent=new Intent(this,DisplayActivity.class);
+          startActivity(intent);
+
+
+    }
+}
